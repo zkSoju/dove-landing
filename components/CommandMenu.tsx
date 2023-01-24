14383361306e27dc9f1ea561a6ef5dfd3c7fa2c4
Tsunfire <underrated@gmail.com>
2023-01-24 14:41:12 -0800

    pt5 idek

--- components/CommandMenu.tsx
+++ components/CommandMenu.tsx
@@ -22,32 +22,43 @@ const CommandMenu = () => {
       open={true}
       onOpenChange={setOpen}
       label="Global Command Menu"
-      className="absolute left-1/2 top-1/2 z-[100] w-full max-w-xl -translate-x-1/2 -translate-y-1/2 rounded-sm border border-[#c4fb60] bg-black p-1"
+      className="absolute left-1/2 top-1/2 z-[100] w-full max-w-2xl -translate-x-1/2 -translate-y-1/2 p-2"
     >
-      <Command.Input
-        placeholder="Type a command or search..."
-        className="w-full border-b border-[#c4fb60] bg-transparent p-6 text-white placeholder:text-white/50 focus:outline-none"
+      <img
+        src="/frame.svg"
+        className="pointer-events-none absolute top-0 left-0 h-full"
       />
-      <div className="flex h-full w-full pt-2">
-        <Command.List className="w-full">
-          <Command.Empty>No results found.</Command.Empty>
+      <div className="rounded-sm border border-[#c4fb6050] bg-black p-1">
+        <Command.Input
+          placeholder="Type a command or search..."
+          className="w-full border-b border-[#c4fb60] bg-transparent p-6 text-sm uppercase tracking-widest text-white placeholder:text-white/50 focus:outline-none"
+        />
+        <div className="flex h-full w-full p-2 px-4">
+          <Command.List className="w-full">
+            <Command.Empty>No results found.</Command.Empty>
 
-          <Command.Group className="text-white">
-            <Command.Item className="rounded-sm px-4 py-4 text-white/50 hover:bg-[#c4fb6050] hover:font-medium hover:text-white">
-              Provide liquidity
-            </Command.Item>
-            <Command.Item className="rounded-sm px-4 py-4 text-white/50 hover:bg-[#c4fb6050] hover:font-medium hover:text-white">
-              Swap assets
-            </Command.Item>
-            <Command.Separator />
-            <Command.Item className="rounded-sm px-4 py-4 text-white/50 hover:bg-[#c4fb6050] hover:font-medium hover:text-white">
-              Learn more
-            </Command.Item>
-          </Command.Group>
-          {/* 
+            <Command.Group className="text-white">
+              <Command.Item className="rounded-sm border border-r-0 border-transparent px-4 py-4 text-sm uppercase tracking-widest text-white/50 hover:border-[#c4fb60] hover:font-medium hover:text-white">
+                Provide liquidity
+              </Command.Item>
+              <Command.Item className="rounded-sm border border-r-0 border-transparent px-4 py-4 text-sm uppercase tracking-widest text-white/50 hover:border-[#c4fb60] hover:font-medium hover:text-white">
+                Swap assets
+              </Command.Item>
+              <Command.Separator />
+              <Command.Item className="rounded-sm border border-r-0 border-transparent px-4 py-4 text-sm uppercase tracking-widest text-white/50 hover:border-[#c4fb60] hover:font-medium hover:text-white">
+                Learn more
+              </Command.Item>
+            </Command.Group>
+            {/* 
         <Command.Item>Apple</Command.Item> */}
-        </Command.List>
-        <div className="h-96 w-full border border-[#c4fb60]"></div>
+          </Command.List>
+          <div className="relative h-64 w-full rounded-sm border border-[#c4fb60]">
+            <img
+              src="/frame_2.svg"
+              className="pointer-events-none absolute bottom-0 left-0 -translate-x-full"
+            />
+          </div>
+        </div>
       </div>
     </Command.Dialog>
   );
